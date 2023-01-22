4e1ad07a30e0e29bec7c06c0a4a97bef25022c59
Bartek Pawlaczyk <bpawlaczyk1982@gmail.com>
2023-01-22 17:02:18 +0100

    fixed functions in script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 {
     const welcome = () => {
         console.log("Witam wszystkich, którzy ty zaglądają 🤩");
-    }
+    };
     
     const toggleBackground = () => {
+        const container = document.querySelector(".container");
         container.classList.toggle("removeBackground");
         removeBackgroundbutton.innerText = container.classList.contains("removeBackground") ? "Przywróć tło strony" : "Usuń tło strony";
-    }
+    };
     
     const togglePhoto = () => {
+        const photo = document.querySelector(".photo");
         photo.classList.toggle("hidden");
         removePhotobutton.innerText = photo.classList.contains("hidden") ? "Przywróć zdjęcie" : "Usuń zdjęcie";
-    }
+    };
     
     welcome();
     
     const removeBackgroundbutton = document.querySelector(".removeBackground");
-    const container = document.querySelector(".container");
     const removePhotobutton = document.querySelector(".removePhoto");
-    const photo = document.querySelector(".photo");
-    
+        
     removeBackgroundbutton.addEventListener("click", toggleBackground);
     removePhotobutton.addEventListener("click", togglePhoto );
     
