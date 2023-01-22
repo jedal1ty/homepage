793903d7a712945d92af781992c1b3906035aabb
Bartek Pawlaczyk <bpawlaczyk1982@gmail.com>
2023-01-22 23:41:50 +0100

    corrected code

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 {
     const welcome = () => {
         console.log("Witam wszystkich, którzy ty zaglądają 🤩");
-    };
-    
+    }
+
     const toggleBackground = () => {
         const container = document.querySelector(".container");
+
         container.classList.toggle("removeBackground");
         removeBackgroundbutton.innerText = container.classList.contains("removeBackground") ? "Przywróć tło strony" : "Usuń tło strony";
-    };
-    
+    }
+
     const togglePhoto = () => {
         const photo = document.querySelector(".photo");
+        
         photo.classList.toggle("hidden");
         removePhotobutton.innerText = photo.classList.contains("hidden") ? "Przywróć zdjęcie" : "Usuń zdjęcie";
-    };
-    
+    }
+
     welcome();
-    
+
     const removeBackgroundbutton = document.querySelector(".removeBackground");
     const removePhotobutton = document.querySelector(".removePhoto");
-        
+
+
     removeBackgroundbutton.addEventListener("click", toggleBackground);
-    removePhotobutton.addEventListener("click", togglePhoto );
-    
+    removePhotobutton.addEventListener("click", togglePhoto);
+
 }
